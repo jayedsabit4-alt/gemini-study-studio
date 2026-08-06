739e21c06768383cf05bae4a721ef5da9638dbf2
jayedsabit4-alt <jayedsabit4@gmail.com>
2026-08-06 20:16:08 +0600

    Update __init__.py to include exam module exports

--- exam/__init__.py
+++ exam/__init__.py
@@ -1,1 +1,15 @@
+"""Exam Generation and Evaluation Package API."""
 
+from .evaluator import evaluate_written_submission
+from .mcq import generate_mcq_paper, score_mcq_submission
+from .timer import ExamTimer
+from .written import generate_written_question_paper, grade_written_exam
+
+__all__ = [
+    "generate_mcq_paper",
+    "score_mcq_submission",
+    "generate_written_question_paper",
+    "grade_written_exam",
+    "evaluate_written_submission",
+    "ExamTimer",
+]
