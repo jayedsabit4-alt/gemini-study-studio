70df35d3b399cbbc4665e45560e85da792944879
jayedsabit4-alt <jayedsabit4@gmail.com>
2026-08-06 20:15:50 +0600

    Add dashboard functions to analytics package API

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,1 +1,8 @@
+"""Analytics and Dashboard Metrics Package API."""
 
+from .dashboard import calculate_study_streak, get_dashboard_summary
+
+__all__ = [
+    "calculate_study_streak",
+    "get_dashboard_summary",
+]
