20ee158a527aa1a40ad98aca4c9d69c7ef87c7f7
jayedsabit4-alt <jayedsabit4@gmail.com>
2026-08-06 20:56:04 +0600

    Update __init__.py

--- mistakes/__init__.py
+++ mistakes/__init__.py
@@ -3,12 +3,13 @@
 from .adaptive_revision import generate_adaptive_revision_notes
 from .manager import calculate_sm2, update_mistake_review
 from .mastery import calculate_subject_mastery
-from .tracker import get_due_mistakes, log_mistake
+from .tracker import get_due_mistakes, log_mistake, record_question_attempt
 
 __all__ = [
     "calculate_sm2",
     "update_mistake_review",
     "log_mistake",
+    "record_question_attempt",
     "get_due_mistakes",
     "calculate_subject_mastery",
     "generate_adaptive_revision_notes",
